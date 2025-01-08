c612802c2d4765b6713c9dbfa6f1a7895af9aa65
Armands Grinbergs-Ozols <55361950+amixslv@users.noreply.github.com>
2025-01-08 12:57:23 +0100

    Add files via upload

---
+++ about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <H1>About Us</H1>
+    <p><strong>Our Mission:</strong> "Tech Innovators" is committed to creating future technologies that enhance people's quality of life and promote sustainable development. We work on innovative solutions that help businesses and individuals achieve their goals more efficiently and in an environmentally friendly manner.</p>
+    <p><strong>Our Values:</strong> We believe that innovation, collaboration, and responsibility are the key factors driving our success. Our team consists of talented professionals who are passionate about technology and dedicated to making a positive impact on the world.</p>
+</body>
+</html>
